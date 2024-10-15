018f7fea8e2046cb0a1c3fdc05c82fb6e81c65e6
Despereaux <rlaehdwn025@naver.com>
2024-10-16 08:53:42 +0900

    Initial commit

---
+++ src/test/java/com/despereaux/jpascheduler/JpaSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despereaux.jpascheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
